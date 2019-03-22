9ddfadcdaee54c5b5ceb03809c295fc42015cebc
Daniel Jiang <2433719J@student.gla.ac.uk>
2019-03-22 14:31:41 +0000

    Likes limited, no styling

--- static/js/dare2ask-ajax.js
+++ static/js/dare2ask-ajax.js
@@ -2,14 +2,19 @@
 $('.likes').click(function(){
     var upvoteid;
     upvoteid = $(this).attr("data-upvoteid");
-    console.log("qid="+upvoteid);
+    //console.log("qid="+upvoteid);
     $.get('/dare2ask/like/', {like_id: upvoteid}, function(data){ //like_id passing to views.py
         // Code to display the updated number of likes for every question in the view.
-        console.log(data);
+        //console.log(data);
         var likes = data.split(" ");
         $('.like_count').each(function(index) {
           $(this).text(likes[index]);
         });
-        //$('#likes').hide();
+        // Disable button after click
+        var el = $('.likes').filter( function () {
+          return $(this).attr("data-upvoteid") == upvoteid
+        });
+        el.text("Liked");
+        el.attr("disabled", true);
   });
 });
